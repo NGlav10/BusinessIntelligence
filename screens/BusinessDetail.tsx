f7548f8e54ff8bbc4370d333e307ad84212fcf8d
Nglav10 <nglaviano10@gmail.com>
2022-04-07 20:02:21 -0500

    Convert to typescript and functional components

---
+++ screens/BusinessDetail.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import {View} from 'react-native';
+
+const BusinessDetail = () => <View />;
+
+export default BusinessDetail;
---
+++ screens/Businesses.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { FlatList, Text } from 'react-native';
+
+const Businesses = () => <Text>Foo bar shibby</Text>;
+
+export default Businesses;
