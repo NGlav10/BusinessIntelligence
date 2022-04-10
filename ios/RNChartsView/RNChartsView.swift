9b1adf9bc75b715309613f1a9b4d280f7030e550
Nglav10 <nglaviano10@gmail.com>
2022-04-10 08:37:57 -0500

    Update Chart UI and organize code

--- ios/RNChartsView/RNChartsView.swift
+++ ios/RNChartsView/RNChartsView.swift
@@ -14,48 +14,58 @@ class RNChartsView: UIView {
   
   var chartView = HorizontalBarChartView()
   
-  private func styleDataSet(dataSet: BarChartDataSet) {
-    dataSet.highlightEnabled = false
-    dataSet.colors = ChartColorTemplates.pastel()
+  func formatChart() {
+    chartView.legend.enabled = false
+    chartView.minOffset = 0
+    chartView.extraTopOffset = 15
+    chartView.animate(yAxisDuration: 1.0)
   }
   
-  private func styleChart() {
-    chartView.rightAxis.enabled = false
-    chartView.legend.enabled = false
+  func formatRightAxis() {
+    let rightAxis = chartView.rightAxis
+    rightAxis.enabled = false
+    rightAxis.axisMinimum = 0
+  }
+  
+  func formatLeftAxis() {
+    let leftAxis = chartView.leftAxis
+    leftAxis.axisMinimum = 0
     
     let currencyFormatter = NumberFormatter()
     currencyFormatter.numberStyle = .currency
     currencyFormatter.minimumFractionDigits = 0
     currencyFormatter.positiveSuffix = "M"
-    currencyFormatter.negativeSuffix = "M"
-    chartView.leftAxis.valueFormatter = DefaultAxisValueFormatter(formatter: currencyFormatter)
-    chartView.leftAxis.labelAlignment = .natural
-    chartView.leftAxis.axisMinimum = 0
-    
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateStyle = .short
-    dateFormatter.timeStyle = .none
-    chartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: xValues)
-    chartView.xAxis.drawGridLinesEnabled = false
-    chartView.xAxis.labelPosition = .bottom
-    
-    chartView.animate(xAxisDuration: 0, yAxisDuration: 1.5)
+    leftAxis.valueFormatter = DefaultAxisValueFormatter(formatter: currencyFormatter)
+    leftAxis.labelFont = UIFont(name: "AppleSDGothicNeo-Light", size: 10.0)!
+  }
+  
+  func formatXAxis() {
+    let xAxis = chartView.xAxis
+    let dateValues = formatDate(dates: xValues)
+    xAxis.valueFormatter = IndexAxisValueFormatter(values: dateValues)
+    xAxis.drawGridLinesEnabled = false
+    xAxis.labelPosition = .bottom
+    xAxis.labelFont = UIFont(name: "AppleSDGothicNeo-Light", size: 10.0)!
   }
   
   private func initializeChart() {
-    var barChartDataEntries: [BarChartDataEntry] = []
-    for (index, value) in yValues.reversed().enumerated() {
+    let barChartDataEntries = yValues.reversed().enumerated().map({(index, value) -> BarChartDataEntry in
       let abbreviatedValue = value / 1000000
-      let dataEntry = BarChartDataEntry(x: Double(index), y: abbreviatedValue)
-      barChartDataEntries.append(dataEntry)
-    }
+      return BarChartDataEntry(x: Double(index), y: abbreviatedValue)
+    })
     
     let dataSet = BarChartDataSet(entries: barChartDataEntries, label: "Revenue")
-    self.styleDataSet(dataSet: dataSet)
+    dataSet.highlightEnabled = false
+    dataSet.colors = ChartColorTemplates.pastel()
+    dataSet.valueFont = UIFont(name: "AppleSDGothicNeo-Medium", size: 10.0)!
+    
+    formatChart()
+    formatRightAxis()
+    formatLeftAxis()
+    formatXAxis()
+    
     chartView.data = BarChartData(dataSet: dataSet)
-    
-    self.styleChart()
-    
+    chartView.notifyDataSetChanged()
     self.addSubview(chartView)
   }
   
@@ -64,4 +74,15 @@ class RNChartsView: UIView {
     chartView.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height)
     self.initializeChart()
   }
+}
+
+func formatDate(dates: [String] ) -> [String] {
+  let oldDateFormatter = DateFormatter()
+  oldDateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+  let dateFormatter = DateFormatter()
+  dateFormatter.dateFormat = "MMM yyyy"
+  return dates.reversed().map({(date: String) -> String in
+    let oldDate = oldDateFormatter.date(from: date)
+    return dateFormatter.string(from: oldDate!).replacingOccurrences(of: " ", with: "\n")
+  })
 }
